14764bc9177d91327e71140dcafca6a7dcbd9cc4
JNPN7 <jpphanju54@gmail.com>
2020-10-22 17:29:43 -0400

    removed bootstrp

---
+++ webtech/includes/footer.php
@@ -0,0 +1,45 @@
+    <!-- <div class="f-line"></div>
+    <section id="footer">
+        <div class="container">
+          <div class="f-space">
+            <div class="row">
+                <div class="col-md-3"><br>
+                    <i class="fa fa-robot" aria-hidden="true" style="color:#fff; font-size:70px;"></i> <b class="nname" style="color: #fff;">Techx</b>
+                </div>
+                <div class="col-md-3"><br>
+                  <h6 class="f-header" style="font-weight: 700;">DEVELOPED BY</h6>
+                  <p class="texts">
+                      <i class="fa fa-user" aria-hidden="true"></i>  Abhinandan Shrestha<br>
+                      <i class="fa fa-user" aria-hidden="true"></i>  Bibek Manandhar<br>
+                      <i class="fa fa-user" aria-hidden="true"></i>  Sudip Manandhar<br>
+                      <i class="fa fa-user" aria-hidden="true"></i>  Juhel Phanju<br><br>
+                  <a href="#"><i class="fa fa-facebook-official"></i></a>
+                  <a href="#"><i class="fa fa-snapchat-ghost"></i></a>
+                  <a href="#"><i class="fa fa-instagram"></i></a>
+                  <a href="#"><i class="fa fa-twitter-square"></i></a>
+                  <a href="#"><i class="fa fa-github-square"></i></a>
+                  </p>
+                </div>  
+                <div class="col-md-3"><br>
+                 <h6 class="f-header" style="font-weight: 700;">QUICK LINKS</h6><p class="texts">
+                  <a class="f-link" href="#">NEWS</a><br>
+                  <a class="f-link" href="#">GAMING</a><br>
+                  <a class="f-link" href="#">ABOUT</a><br>
+                </div>    
+                <div class="col-md-3"><br>
+                  <h6 class="f-header" style="font-weight: 700;">COMMUNITY</h6><p class="texts">
+                   <a class="f-link" href="#">SUPPORT</a><br>
+                   <a class="f-link" href="#">COMMUNITY GUIDELINES</a><br>
+                   <a class="f-link" href="#">PRIVACY & SECURITY</a><br>
+                 </div>    
+            </div>
+          </div>
+        </div>
+    </section>
+    <div class="end">
+      <div class="end-text">
+        Copyright <i class="fa fa-copyright" aria-hidden="true"></i> 2020
+      </div>
+    </div> -->
+	</body>
+</html>
--- webtech/index.php
+++ webtech/index.php
@@ -1,28 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<!-- Title -->
-		<title>WEBTECH</title>
-
-		<!-- Custom stlylesheet -->
-		<link type="text/css" rel="stylesheet" href="assets/css/style.css"/>
-
-		<!-- Bootstrap CSS -->
-    	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-	</head>
-	<body>
-
-		<!------------navbar section----------->
-
-		
-
+<?php
+	include 'includes/header.php';
+?>
 		<section class="hot-news">
 			<div class="row">
 				<div class="hot-news-left">
-					<div class="post h-50">
+					<div class="post ht-50">
 						<img class="post-thumb" src="/assets/images/mountain1.jpeg" alt="Snow" >
 						<div class="post-meta">
 							<div class="post-date">Jan 2, 2020</div>
@@ -31,7 +13,7 @@
 					</div>
 				</div>
 				<div class="hot-news-right">
-					<div class="post h-50">
+					<div class="post ht-50">
 						<img class="post-thumb" src="/assets/images/mountain3.jpeg" alt="Snow" style="width:100%;">
 						<div class="post-meta">
 							<div class="post-date">May 13, 2020</div>
@@ -49,7 +31,7 @@
 						<h1 style="font-size: 35px">Trending</h1>
 						<div class="grid-container">
 							<div class="grid-item1">
-								<div class="post h-50">
+								<div class="post ht-50">
 									<img class="post-thumb" src="/assets/images/mountain1.jpeg" alt="Snow" style="width:100%;">
 									<div class="post-meta">
 										<div class="post-date">May 13, 2020</div>
@@ -58,7 +40,7 @@
 								</div>
 							</div>
 							<div class="grid-item2">
-								<div class="post h-50">
+								<div class="post ht-50">
 									<img class="post-thumb" src="/assets/images/mountain2.jpeg" alt="Snow" style="width:100%;">
 									<div class="post-meta">
 										<div class="post-date">May 13, 2020</div>
@@ -67,7 +49,7 @@
 								</div>
 							</div>
 							<div class="grid-item3">
-								<div class="post h-100 test">
+								<div class="post ht-100 test">
 									<img class="post-thumb" src="/assets/images/mountain3.jpeg" alt="Snow" style="width:100%;">
 									<div class="post-meta">
 										<div class="post-date">May 13, 2020</div>
@@ -76,7 +58,7 @@
 								</div>
 							</div>
 							<div class="grid-item4">
-								<div class="post h-50">
+								<div class="post ht-50">
 									<img class="post-thumb" src="/assets/images/mountain4.jpeg" alt="Snow" style="width:100%;">
 									<div class="post-meta">
 										<div class="post-date">May 13, 2020</div>
@@ -85,7 +67,7 @@
 								</div>
 							</div>
 							<div class="grid-item5">
-								<div class="post h-50">
+								<div class="post ht-50">
 									<img class="post-thumb" src="/assets/images/mountain2.jpeg" alt="Snow" style="width:100%;">
 									<div class="post-meta">
 										<div class="post-date">May 13, 2020</div>
@@ -103,58 +85,6 @@
 			
 		</main>
 
-		<!-------------foooooter------->
-
-		<div class="f-line"></div>
-		<section id="footer">
-		    <div class="container">
-		      <div class="f-space">
-		        <div class="row">
-		            <div class="col-md-3"><br>
-		                <i class="fa fa-robot" aria-hidden="true" style="color:#fff; font-size:70px;"></i> <b class="nname" style="color: #fff;"">Techx</b>
-		            </div>
-		            <div class="col-md-3"><br>
-		              <h6 class="f-header" style="font-weight: 700;">DEVELOPED BY</h6>
-		              <p class="texts">
-		                  <i class="fa fa-user" aria-hidden="true"></i>  Abhinandan Shrestha<br>
-		                  <i class="fa fa-user" aria-hidden="true"></i>  Bibek Manandhar<br>
-		                  <i class="fa fa-user" aria-hidden="true"></i>  Sudip Manandhar<br>
-		                  <i class="fa fa-user" aria-hidden="true"></i>  Juhel Phanju<br><br>
-		              <a href="#"><i class="fa fa-facebook-official"></i></a>
-		              <a href="#"><i class="fa fa-snapchat-ghost"></i></a>
-		              <a href="#"><i class="fa fa-instagram"></i></a>
-		              <a href="#"><i class="fa fa-twitter-square"></i></a>
-		              <a href="#"><i class="fa fa-github-square"></i></a>
-		              </p>
-		            </div>  
-		            <div class="col-md-3"><br>
-		             <h6 class="f-header" style="font-weight: 700;">QUICK LINKS</h6><p class="texts">
-		              <a class="f-link" href="#">NEWS</a><br>
-		              <a class="f-link" href="#">GAMING</a><br>
-		              <a class="f-link" href="#">ABOUT</a><br>
-		            </div>    
-		            <div class="col-md-3"><br>
-		              <h6 class="f-header" style="font-weight: 700;">COMMUNITY</h6><p class="texts">
-		               <a class="f-link" href="#">SUPPORT</a><br>
-		               <a class="f-link" href="#">COMMUNITY GUIDELINES</a><br>
-		               <a class="f-link" href="#">PRIVACY & SECURITY</a><br>
-		             </div>    
-		        </div>
-		      </div>
-		    </div>
-		</section>
-		<div class="end">
-		  <div class="end-text">
-		  Copyright<i class="fa fa-copyright" aria-hidden="true"></i>2020
-		  </div>
-		</div>
-		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-	    <script>
-	      $(window).scroll(function(){
-	        $('nav').toggleClass('scrolled',$(this).scrollTop > 20);
-	      });
-	    </script>
-	</body>
-</html>
+<?php
+	include 'includes/footer.php';
+?>
