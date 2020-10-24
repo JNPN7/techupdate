7b1218e0a1d75bb141bdc16762272ccca3319de9
JNPN7 <jpphanju54@gmail.com>
2020-10-23 22:55:56 -0400

    about us and contacts

---
+++ webtech/contact.php
@@ -0,0 +1,62 @@
+<?php
+	include 'includes/header.php';
+?>
+
+<main role="main" class="col">
+	<!-- banner -->
+	<section class="banner">
+		<div class="row" style="color: #fff;">
+			<a href="index" class="banner-home"><p>Home</p></a>
+			<div style="width: 15px"></div>
+			<p>/</p>
+			<div style="width: 15px"></div>
+			<p>Contact</p>
+		</div>
+		<h1 style="color: #fff; margin-top: 0;">Contact</h1>
+	</section>
+
+	<!-- main content -->
+	<section class="row">
+		<!-- main section -->
+		<article class="main-contact">
+			<div class="main-pad">
+				<h3>Contact Information</h3>
+				<p>Oh! So, you wanna contact me. Why? I don't see any reason for you to contact me. Why I wanna get in contact with this world burden, who should be dead, not tying to contact me. Ok! Ok! Don't cry! I have left some of my detail down there. Contact me just before dying, I need some reason to celebrate!!</p>
+				<ul class="list-style">
+					<li><p><strong>Email:</strong> <a href="#"> jpphanju54@gmail.com</a></p></li>
+					<li><p><strong>Phone:</strong> Dream About it</p></li>
+					<li><p><strong>Address:</strong> Hahaha Why??</p></li>
+				</ul>
+			</div>
+		</article>
+
+		<!-- right section -->
+		<aside class="right-contact">
+			<div class="main-pad col" style="padding-top: 20px;">
+				<h2>Send A Message</h2>
+				<form>  
+					<div class="col" style="padding-right: 50px;">
+
+						<h3 style="margin-bottom: 0;">Email</h3>
+						<input class="input" type="email" name="email" required=""/>
+	
+						<h3 style="margin-bottom: 0; margin-top: 15px">Subject</h3>
+						<input class="input" type="text" name="subject" required=""/>
+
+						<div style="height: 20px"></div>
+						<textarea class="input" style="height: 100px; padding-top: 5px" name="message" placeholder="Message" required=""/></textarea>
+
+						<div style="height: 20px"></div>
+						<button class="primary-button" style="width: 140px">Submit</button>
+						
+					</div>
+				</form>
+			</div>
+		</aside>
+	</section>
+</main>
+
+
+<?php
+	include 'includes/footer.php';
+?>
