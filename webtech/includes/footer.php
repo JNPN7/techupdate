a15e13f08cfc61e40aab7bebf0bbb5e41eed8cca
abhinandanshrestha <shtabhi@gmail.com>
2020-10-24 13:01:23 +0545

    footer banaye

--- webtech/includes/footer.php
+++ webtech/includes/footer.php
@@ -1,46 +1,55 @@
-    <!-- <div class="f-line"></div>
-    <section id="footer">
-        <div class="container">
-          <div class="f-space">
-            <div class="row">
-                <div class="col-md-3"><br>
-                    <i class="fa fa-robot" aria-hidden="true" style="color:#fff; font-size:70px;"></i> <b class="nname" style="color: #fff;">Techx</b>
-                </div>
-                <div class="col-md-3"><br>
-                  <h6 class="f-header" style="font-weight: 700;">DEVELOPED BY</h6>
-                  <p class="texts">
-                      <i class="fa fa-user" aria-hidden="true"></i>  Abhinandan Shrestha<br>
-                      <i class="fa fa-user" aria-hidden="true"></i>  Bibek Manandhar<br>
-                      <i class="fa fa-user" aria-hidden="true"></i>  Sudip Manandhar<br>
-                      <i class="fa fa-user" aria-hidden="true"></i>  Juhel Phanju<br><br>
-                  <a href="#"><i class="fa fa-facebook-official"></i></a>
-                  <a href="#"><i class="fa fa-snapchat-ghost"></i></a>
-                  <a href="#"><i class="fa fa-instagram"></i></a>
-                  <a href="#"><i class="fa fa-twitter-square"></i></a>
-                  <a href="#"><i class="fa fa-github-square"></i></a>
-                  </p>
-                </div>  
-                <div class="col-md-3"><br>
-                 <h6 class="f-header" style="font-weight: 700;">QUICK LINKS</h6><p class="texts">
-                  <a class="f-link" href="#">NEWS</a><br>
-                  <a class="f-link" href="#">GAMING</a><br>
-                  <a class="f-link" href="#">ABOUT</a><br>
-                </div>    
-                <div class="col-md-3"><br>
-                  <h6 class="f-header" style="font-weight: 700;">COMMUNITY</h6><p class="texts">
-                   <a class="f-link" href="#">SUPPORT</a><br>
-                   <a class="f-link" href="#">COMMUNITY GUIDELINES</a><br>
-                   <a class="f-link" href="#">PRIVACY & SECURITY</a><br>
-                 </div>    
+          <div class="f-line"></div>
+          <section id="footer">
+              <div class="container">
+                  <div class="row">
+                      <div class="col-md-3"><div class="f-space"></div>
+                          <i class="fa fa-robot" aria-hidden="true" style="color:#fff; font-size:70px;"></i> <b class="nname" style="color: #fff;">Techx</b>
+                      </div>
+                      <div class="col-md-3"><br>
+                        <h6 class="f-header">DEVELOPED BY</h6>
+                        <p class="texts">
+                            Abhinandan Shrestha<br>
+                            Bibek Manandhar<br>
+                            Sudip Manandhar<br>
+                            Juhel Phanju<br><br>
+                        <a href="#"><i class="fa fa-facebook-official"></i></a>
+                        <a href="#"><i class="fa fa-snapchat-ghost"></i></a>
+                        <a href="#"><i class="fa fa-instagram"></i></a>
+                        <a href="#"><i class="fa fa-twitter-square"></i></a>
+                        <a href="#"><i class="fa fa-github-square"></i></a>
+                        </p>
+                      </div>  
+                      <div class="col-md-3"><br>
+                      <h6 class="f-header">QUICK LINKS</h6>
+                        <a class="f-link texts" href="#">NEWS</a><br>
+                        <a class="f-link texts" href="#">GAMING</a><br>
+                        <a class="f-link texts" href="#">ABOUT</a><br>
+                      </div>    
+                      <div class="col-md-3"><br>
+                        <h6 class="f-header">COMMUNITY</h6>
+                        <a class="f-link texts" href="#">SUPPORT</a><br>
+                        <a class="f-link texts" href="#">COMMUNITY GUIDELINES</a><br>
+                        <a class="f-link texts" href="#">PRIVACY & SECURITY</a><br>
+                      </div>
+                      <div class="col-md-3"><br>
+                      <form action="">
+                        <h6 class="f-header">Join Newsletter</h6>
+                        <p class="texts" href="#">Get email about our latest updates</p>
+                        <div class="emailbox">
+                          <i class="fa fa-envelope"></i>
+                          <input class="tbox" type="email" name="" value="" placeholder="Enter your email...">
+                          <button class="btn" type="button" name="button">Subscribe</button>
+                        </div>
+                        </form>
+                      </div>      
+                  </div>
+              </div>
+              <div class="f-space"></div>
+          </section>
+          <div class="end">
+            <div class="end-text">
+            Copyright<i class="fa fa-copyright" aria-hidden="true"></i>2020
             </div>
           </div>
-        </div>
-    </section>
-    <div class="end">
-      <div class="end-text">
-        Copyright <i class="fa fa-copyright" aria-hidden="true"></i> 2020
-      </div>
-    </div> -->
 	</body>
-  <script src='assets/js/main.js'></script>
 </html>
