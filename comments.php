fca409d6c1442805d41234f71043ea25467d8d1b
Bibek Manandhar <manandhar.bibek01@gmail.com>
2020-11-02 18:33:52 +0545

    comment frontend

---
+++ comments.php
@@ -0,0 +1,58 @@
+<div id="commentContainer">
+    <div class="commentNumber">3 comments</div>
+    <div class="comment">
+        <div class="commentContent">
+            <div class="commentatorImage">
+                <img src="assets\images\bg.jpeg" alt="">
+            </div>
+            <div class="commentDetails">
+                <p class="commentator">Anonymous</p>
+                <p class="commentTime">Jun 07, 2020 10:06 am<button onclick="" class="replyButton">Reply</button></p>
+                <p class="commentDescription">Captain Fantastico ???????????</p>
+            </div>
+        </div>
+        <div class="reply commentContent">
+            <div class="commentatorImage">
+                <img src="assets\images\bg.jpeg" alt="">
+            </div>
+            <div class="commentDetails">
+                <p class="commentator">Anonymous</p>
+                <p class="commentTime">Jun 07, 2020 10:06 am<button class="replyButton">Reply</button></p>
+                <p class="commentDescription">Captain Fantastico ???????????</p>
+            </div>
+        </div>
+    </div>
+    <div class="comment">
+        <div class="commentContent">
+            <div class="commentatorImage">
+                <img src="assets\images\bg.jpeg" alt="">
+            </div>
+            <div class="commentDetails">
+                <p class="commentator">Anonymous</p>
+                <p class="commentTime">Jun 07, 2020 10:06 am<button class="replyButton">Reply</button></p>
+                <p class="commentDescription">Captain Fantastico ???????????</p>
+            </div>
+        </div>
+    </div>
+    <div class="leaveAReply">
+        <div class="heading">LEAVE A REPLY</div>
+        <input placeholder="Type a message." type="text">        
+    </div>
+</div>
+<div id="replyBox">
+    <div class="replyForm">
+            <form action="#">
+                <div class="message">
+                    <textarea name="" id="" cols="30" rows="10" placeholder="Message"></textarea>
+                </div>
+                <div>
+                    <button class="cancelButton">
+                        Cancel
+                    </button>
+                    <button type="submit" class="submitButton">
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </div>
+</div>
