b4c913c9f3621fbe9b37bff57162d83c48123ed7
ManandharSudip4 <manandharsudip8@gmail.com>
2020-11-05 20:19:00 +0545

    minorchanges3

--- includes/header.php
+++ includes/header.php
@@ -31,7 +31,8 @@
 		<!-- Custom stlylesheet -->
 		<link type="text/css" rel="stylesheet" href="assets/css/style.css?pwen"/>
         <link type="text/css" rel="stylesheet" href="assets/css/responsive.css?pwen"/>
-		<!-------------------------------------------->
+
+        <!-------------------------------------------->
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 		<!--[if lt IE 9]>
